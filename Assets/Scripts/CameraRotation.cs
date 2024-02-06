eca354e1ed74ac1375c0cabe33470651188d59f8
Kiriushonok <158612403+Kiriushonok@users.noreply.github.com>
2024-02-06 20:51:18 +0400

    Really added camera rotation

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+    public Transform cameraAxisTransform;
     public float rotationSpeed;
+    public float minAngle;
+    public float maxAngle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,14 @@ public class CameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * rotationSpeed * Input.GetAxis("Mouse X"), 0);
+        transform.localEulerAngles = new Vector3(0,transform.localEulerAngles.y + Time.deltaTime * rotationSpeed * Input.GetAxis("Mouse X"), 0);
+
+        var newAngleX = cameraAxisTransform.localEulerAngles.x - Time.deltaTime * rotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX >= 180)
+            newAngleX -= 360;
+
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+
+        cameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
     }
 }
