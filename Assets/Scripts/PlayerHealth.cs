33dd9d0575f860cebb94ce719734efbfe11d40d6
Kiriushonok <158612403+Kiriushonok@users.noreply.github.com>
2024-02-29 20:36:46 +0400

    It dies

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public float health = 10;
+    public RectTransform valueRectTransform;
+    public GameObject gameOverScreen;
+    public GameObject gameplayUI;
+
+    private float _maxHealth;
 
     private void Death()
     {
-        Destroy(gameObject);
+        gameOverScreen.SetActive(true);
+        gameplayUI.SetActive(false);
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<FireballCaster>().enabled = false;
+        GetComponent<CameraRotation>().enabled = false;
     }
 
     public void Injure(float damage)
@@ -18,5 +27,17 @@ public class PlayerHealth : MonoBehaviour
         {
             Death();
         }
+        DrawHealthBar();
+    }
+
+    private void DrawHealthBar()
+    {
+        valueRectTransform.anchorMax = new Vector2(health / _maxHealth, 1);
+    }
+
+    private void Start()
+    {
+        _maxHealth = health;
+        DrawHealthBar();
     }
 }
