1132a908e500c490ec97d816b788341b76922861
Kiriushonok <158612403+Kiriushonok@users.noreply.github.com>
2024-03-20 22:12:40 +0400

    ггг
    This reverts commit 737fd20480b1e08f361e63224ac17f7dffe01a58.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,9 +9,8 @@ public class EnemyAI : MonoBehaviour
     public PlayerController player;
     public float viewAngle;
     public float damage = 20;
-    public float attackDistance = 0.65f;
-    public bool _isPlayerNoticed;
 
+    public bool _isPlayerNoticed;
     private NavMeshAgent _navMeshAgent;
     private PlayerHealth _playerHealth;
 
@@ -41,10 +40,8 @@ public class EnemyAI : MonoBehaviour
 
     private void EnemyVisionUpdate()
     {
-        
-        _isPlayerNoticed = false;
-        if (!_playerHealth.IsAlive()) return;
         var ditection = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
 
         if (Vector3.Angle(transform.forward, ditection) < viewAngle)
         {
@@ -62,22 +59,27 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackUpdate()
     {
-        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance && _isPlayerNoticed) 
+        if (_isPlayerNoticed){
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance) 
         {
+<<<<<<< HEAD
 <<<<<<< HEAD
             animator.SetBool("attack", true);
             
 =======
             _playerHealth.Injure(damage * Time.deltaTime);
 >>>>>>> parent of cbd3b82 (Started adding goblin with knifes animations)
+=======
+                _playerHealth.Injure(damage * Time.deltaTime);
+            }
+>>>>>>> parent of 737fd20 (Finished goblin with knifes animations)
         }
     }
 
     public void MobAttack()
     {
-        animator.SetBool("attack", false);
         if (!_isPlayerNoticed) return;
-        if (_navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance + attackDistance) return;
+        if (_navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance) return;
         _playerHealth.Injure(damage);
     }
 
