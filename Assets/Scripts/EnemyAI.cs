6fb99429d6b87fa4e5975b33d92d415f3925ccce
Kiriushonok <158612403+Kiriushonok@users.noreply.github.com>
2024-03-20 22:45:42 +0400

    Revert "Finished goblin with knifes animations"
    This reverts commit 5bb08f245d6e80505b391d5409b9bb92b8aa9fc5.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,12 +5,15 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+
+    public Animator animator;
     public List<Transform> patrolPoints;
     public PlayerController player;
     public float viewAngle;
     public float damage = 20;
-
+    public float attackDistance = 0.65f;
     public bool _isPlayerNoticed;
+
     private NavMeshAgent _navMeshAgent;
     private PlayerHealth _playerHealth;
 
@@ -40,8 +43,10 @@ public class EnemyAI : MonoBehaviour
 
     private void EnemyVisionUpdate()
     {
-        var ditection = player.transform.position - transform.position;
+        
         _isPlayerNoticed = false;
+        if (!_playerHealth.IsAlive()) return;
+        var ditection = player.transform.position - transform.position;
 
         if (Vector3.Angle(transform.forward, ditection) < viewAngle)
         {
@@ -59,32 +64,18 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackUpdate()
     {
-        if (_isPlayerNoticed){
-        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance) 
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance && _isPlayerNoticed) 
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             animator.SetBool("attack", true);
             
-=======
-            _playerHealth.Injure(damage * Time.deltaTime);
->>>>>>> parent of cbd3b82 (Started adding goblin with knifes animations)
-=======
-                _playerHealth.Injure(damage * Time.deltaTime);
-            }
->>>>>>> parent of 737fd20 (Finished goblin with knifes animations)
-=======
-                _playerHealth.Injure(damage * Time.deltaTime);
-            }
->>>>>>> parent of 737fd20 (Finished goblin with knifes animations)
         }
     }
 
     public void MobAttack()
     {
+        animator.SetBool("attack", false);
         if (!_isPlayerNoticed) return;
-        if (_navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance) return;
+        if (_navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance + attackDistance) return;
         _playerHealth.Injure(damage);
     }
 
